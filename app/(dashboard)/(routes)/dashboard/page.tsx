4861cdb253f4dd354518e9448c25c2f8a72b9bec
Sarthak <mayhemk2002@gmail.com>
2023-09-28 18:58:15 +0530

    initial commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+import {Card} from "@/components/ui/card"
+import { cn } from "@/lib/utils";
+import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const DashboardPage = () => {
+  const router = useRouter();
+  const tools = [
+    {
+      label : "Conversation",
+      icon : MessageSquare,
+      href : "/conversation",
+      bgColor : "bg-violet-500/10",
+      color : "text-violet-500"
+    },
+    {
+      label : "Music Generation",
+      icon : Music,
+      href : "/music",
+      bgColor : "bg-emerald-500/10",
+      color : "text-emerald-500"
+    },
+    {
+      label : "Image Generation",
+      icon : ImageIcon,
+      href : "/image",
+      bgColor : "bg-pink-700/10",
+      color : "text-pink-700"
+    },
+    {
+      label : "Video Generation",
+      icon : VideoIcon,
+      href : "/video",
+      bgColor : "bg-orange-700/10",
+      color : "text-orange-700"
+    },
+    {
+      label : "Code generation",
+      icon : Code,
+      href : "/code",
+      bgColor : "bg-green-700/10",
+      color : "text-green-700"
+    }
+  ]
+  return (
+    <div>
+        <div className="mb-8 space-y-4">
+          <h2 className="text-2xl md:text-4xl font-bold text-center">
+            Explore the power of AI
+          </h2>
+          <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+            Interact with the smartest AI
+          </p>
+          <div className = "px-4 md:px-20 lg:px-32 space-y-4">
+              {tools.map((tool) => (
+                <Card 
+                onClick={() => {router.push(tool.href)}}
+                key = {tool.href}
+                className = "p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
+                  <div className="flex items-center gap-x-4">
+                    <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                      <tool.icon className={cn("w-8 h-8", tool.color)} />
+                    </div>
+                    <div className="font-semibold">
+                      {tool.label}
+                    </div>
+                  </div>
+                  <ArrowRight className="w-5 h-5" />
+                </Card>
+              ))}
+          </div>
+        </div>
+    </div>
+    
+
+  )
+}
+export default DashboardPage;
---
+++ components/sidebar.tsx
@@ -0,0 +1,100 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { Montserrat } from "next/font/google";
+import {usePathname} from "next/navigation";
+
+import {cn} from "@/lib/utils"
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+import { getApiLimitCount } from "@/lib/api-limit";
+import { FreCounter } from "./free-counter";
+
+const montserrat = Montserrat({
+    weight : "500",
+    subsets : ["latin"]
+});
+
+const routes = [
+    {
+        label : "Dashboard",
+        icon : LayoutDashboard,
+        href : "/dashboard",
+        color : "text-sky-500"
+    },
+    {
+        label : "Conversation",
+        icon : MessageSquare,
+        href : "/conversation",
+        color : "text-violet-500"
+    },
+    {
+        label : "Image generation",
+        icon : ImageIcon,
+        href : "/image",
+        color : "text-pink-700"
+    },
+    {
+        label : "Video Generation",
+        icon : VideoIcon,
+        href : "/video",
+        color : "text-orange-500"
+    },
+    {
+        label : "Music Generation",
+        icon : Music,
+        href : "/music",
+        color : "text-emerald-500"
+    },
+    {
+        label : "Code generation",
+        icon : Code,
+        href : "/code",
+        color : "text-green-700"
+    },
+    {
+        label : "Settings",
+        icon : Settings,
+        href : "/settings",
+    }
+
+];
+
+interface SideBarProps {
+    apiLimitCount : number;
+};
+
+const Sidebar = ({
+    apiLimitCount = 0
+} : SideBarProps) => {
+    const pathname = usePathname()
+  return (
+    <div className="h-full flex flex-col text-white bg-[#111827] space-y-4 py-4">
+        <div className="py-3 px-3 flex-1">
+            <Link href="/dashboard" className = "flex item-center pl-3 mb-14">
+                <div className="relative w-8 h-8 mr-4">
+                    <Image fill alt = "logo" src = "/logo.png" />
+                </div>
+                <h1 className = {cn("text-2xl font-bold", montserrat.className)}>Genius</h1>
+            </Link>
+            <div className = "space-y-1">
+                {routes.map((route) => (
+                    <Link href = {route.href} key = {route.href} 
+                    className= {
+                        cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                            pathname === route.href ? "text-white bg-white/10" : "text-zinc"
+                        )}
+                    >
+                        <div className = "flex items-center flex-1">
+                            <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                            {route.label}
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+        <FreCounter apiLimitCount = {apiLimitCount} />
+    </div>
+  );
+}
+
+export default Sidebar;
